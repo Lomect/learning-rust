35d09d619e6d944c92bc7effa2566c8c34bf7e87
lome <1209518758@qq.com>
2019-06-11 22:49:44 +0800

    hello world

---
+++ hello_word/target/debug/deps/hello_word-e67ce145b157fc00.d
@@ -0,0 +1,5 @@
+/home/lome/rust_game/learning-rust/hello_word/target/debug/deps/hello_word-e67ce145b157fc00: src/main.rs
+
+/home/lome/rust_game/learning-rust/hello_word/target/debug/deps/hello_word-e67ce145b157fc00.d: src/main.rs
+
+src/main.rs:
---
+++ hello_word/target/debug/hello_word.d
@@ -0,0 +1,1 @@
+/home/lome/rust_game/learning-rust/hello_word/target/debug/hello_word: /home/lome/rust_game/learning-rust/hello_word/src/main.rs

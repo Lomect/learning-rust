c15e11b2571a8de147bd82481675475bb83af1a5
lome <1209518758@qq.com>
2019-06-12 09:16:16 +0800

    variable

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,6 +1,13 @@
+// 常量
+const  MAX_POINTS : u32 = 1000_000;
+
 fn main() {
+    // 不可变变量
     let a = 5;
+    // 可变变量
     let mut x = 6;
     x = 7;
-    println!("Hello, world!");
+    // 隐藏
+    let x = x +1;
+    println!("a: {}, x: {}", a, x);
 }
